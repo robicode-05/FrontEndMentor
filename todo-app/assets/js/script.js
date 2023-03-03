32fa259216021f96096ad66831c86c0a9ad8d3e3
Robin Dervieux <robin.dervieux@outlook.com>
2023-03-03 21:10:34 +0100

    TodoApp: Implement drag&drop

--- todo-app/assets/js/script.js
+++ todo-app/assets/js/script.js
@@ -2,12 +2,12 @@ const tasks = [];
 
 // some default tasks
 // if state = true => the task has been done, false => task is todo
-tasks.push({id: "default_task_0", state: true,  text: "Complete online Javascript course"});
-tasks.push({id: "default_task_1", state: false, text: "Jog around the park 3x"});
-tasks.push({id: "default_task_2", state: false, text: "10 minutes meditations"});
-tasks.push({id: "default_task_3", state: false, text: "Read for 1 hour"});
-tasks.push({id: "default_task_4", state: false, text: "Pick up groceries"});
-tasks.push({id: "default_task_5", state: false, text: "Complete Todo App on Frontend Mentor"});
+tasks.push({id: "default_task_0", order: 0, state: true,  text: "Complete online Javascript course"});
+tasks.push({id: "default_task_1", order: 1, state: false, text: "Jog around the park 3x"});
+tasks.push({id: "default_task_2", order: 2, state: false, text: "10 minutes meditations"});
+tasks.push({id: "default_task_3", order: 3, state: false, text: "Read for 1 hour"});
+tasks.push({id: "default_task_4", order: 4, state: false, text: "Pick up groceries"});
+tasks.push({id: "default_task_5", order: 5, state: false, text: "Complete Todo App on Frontend Mentor"});
 
 
 generateTaskListToDisplay(); // initial drawing
@@ -17,7 +17,7 @@ generateTaskListToDisplay(); // initial drawing
  * @param {Event} event 
  */
 function addNewTask(event) {
-  tasks.push({id: crypto.randomUUID(), state: false, text: event.target.value});
+  tasks.push({id: crypto.randomUUID(), order: tasks.length, state: false, text: event.target.value});
   generateTaskListToDisplay();
   event.target.value = "";
 }
@@ -114,10 +114,13 @@ function generateTaskListToDisplay() {
     listParent.removeChild(listParent.firstChild);
   }
 
-  for (const task of tasks) {
+  // for (const task of tasks) {
+    for (const task of tasks.sort((reference, comparison) => reference.order - comparison.order)) {
     const listItem = document.createElement("li");
     listItem.setAttribute("id", task.id);
     listItem.onclick = (event) => clickEventCatcher(task.id, event);
+    listItem.draggable = true;
+    listItem.ondrag = (event) => drag(task.id, event);
 
     // Create state checkbox
     const listItemCheckbox = document.createElement("input");
@@ -146,4 +149,62 @@ function generateTaskListToDisplay() {
     listParent.appendChild(listItem);
   }  
   updateLeftItemCounter();
+}
+
+
+/* DRAG AND DROP MANAGEMENT */
+
+// array between task id and its y coordinate
+// type: {id: string, y: number }
+const tasksPositions = [];
+generatePositionMap(); // Starting up drag & drop system
+
+function generatePositionMap() {
+  tasksPositions.splice(0);
+  for (const task of tasks) {
+    // get vertical coordinate
+    const taskCLientBound = document.querySelector(`li#${task.id}`).getClientRects()[0];
+    // We detect the change when the cursor reach the half of its neighbour
+    const position = taskCLientBound.y + (taskCLientBound.height / 2);
+    tasksPositions.push({id: task.id, y: position});
+  }
+}
+
+function swap(idA, idB) {
+  const aIndex = tasks.findIndex((t) => t.id === idA);
+  const bIndex = tasks.findIndex((t) => t.id == idB);
+
+  if (aIndex < 0 || bIndex < 0) return;
+
+  const oldAOrder = tasks[aIndex].order;
+  tasks[aIndex].order = tasks[bIndex].order;
+  tasks[bIndex].order = oldAOrder;
+}
+
+/**
+ * 
+ * @param {string} taskId 
+ * @param {DragEvent} event 
+ */
+function drag(taskId, event) {
+  const taskCLientBound = document.querySelector(`li#${taskId}`).getClientRects()[0];
+  // We detect the change when the cursor reach the half of its neighbour
+  const startingPosition = taskCLientBound.y + (taskCLientBound.height / 2);
+  const currentPosition = event.y;
+
+  const delta = startingPosition - currentPosition;
+
+  const crossedTasks = (delta < 0) ?
+    tasksPositions.filter((t) => t.y > startingPosition && t.y < currentPosition) :
+    tasksPositions.filter((t) => t.y < startingPosition && t.y > currentPosition);
+
+  if (crossedTasks.length < 1) return; // did not pass any other task
+
+  // for each crossed tasks, reorder for better animation
+  // we could redraw the list only when all the swap are done for better performance
+  for (const newlyABoveTask of crossedTasks) {
+    swap(taskId, newlyABoveTask.id);
+    generatePositionMap();
+    generateTaskListToDisplay();
+  }
 }
