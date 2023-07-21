d43b01c38c2908583efacc3320a1b316a15a1316
Robin Dervieux <robin.dervieux@outlook.com>
2023-07-22 00:49:24 +0200

    Newslettre: Start working on this new challenge

---
+++ newsletter-sign-up/assets/scripts/index.js
@@ -0,0 +1,23 @@
+function handleEnterKey(e) {
+  console.log("handleEnterKey", e);
+  if( e.key !== 'Enter') return;
+  
+  e.preventDefault();
+  retrieveEmail();
+}
+
+function retrieveEmail() {
+  const userEmail = document.querySelector("#newsletter-email input").value;
+  const isEmailValid = validateEmail(userEmail);
+  if (!isEmailValid)  document.querySelector("#newsletter-email").classList.add("error");
+  else  document.querySelector("#newsletter-email").classList.remove("error");
+}
+
+
+function  validateEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
