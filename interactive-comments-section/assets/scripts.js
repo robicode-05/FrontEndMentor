5ec37c2b1d4e4bafd6c29db95bd1cc0e0fb1b060
Robin Dervieux <robin.dervieux@outlook.com>
2023-07-22 19:41:18 +0200

    CommentSection: Make minimal web component for message

--- interactive-comments-section/assets/scripts.js
+++ interactive-comments-section/assets/scripts.js
@@ -17,7 +17,6 @@ window.onload = async function() {
 
     
 async function retrieveStoredData() {
-  console.log("getCurrentUserData");
   const response = await fetch("assets/data.json");
   const data = await response.json();
 
@@ -32,9 +31,7 @@ function renderMessages() {
 
   for (const comment of comments) {
     const messageCardComponent = createMessageCardComponent(comment);
-    console.log("createMessageCardComponent", messageCardComponent);
     main.appendChild(messageCardComponent); 
-    console.log("appenc main", main);
   }
 }
 
@@ -44,6 +41,23 @@ function renderMessages() {
  */
 function createMessageCardComponent(comment) {
   const messageCardComponent = document.createElement("message-card");
-  messageCardComponent.setAttribute("author", comment.user.username)
+  messageCardComponent.setAttribute("author", comment.user.username);
+  messageCardComponent.setAttribute("created", comment.createdAt);
+  messageCardComponent.appendChild(createMessageContent(comment));
   return messageCardComponent;
+}
+
+/**
+ * @param {Comment} comment
+ * @param {"reader" | "editor"} mode
+ */
+function createMessageContent(comment, mode) {
+  // if (mode === "reader") {
+
+  const messageContent = document.createElement("p");
+  messageContent.setAttribute("slot", "content");
+  messageContent.textContent = comment.content;
+
+  return messageContent;
+  // }
 }
--- interactive-comments-section/components/message-card.js
+++ interactive-comments-section/components/message-card.js
@@ -2,59 +2,99 @@ const template = document.createElement('template');
 template.innerHTML = `
 <style>
   .message-card {
+    display: flex;
+    border: 1px solid black;
+    gap: 1rem;
+  }
+
+  .like-button {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .message-content { 
+    display: flex;
+    flex-direction: column;
+  }
+
+  .header { 
+    display: flex;
     background-color: red;
-    color: white;
-    width: 250px;
-    height: 100px;
+    justify-content: space-between;
   }
 
 </style>
+
 <div class="message-card">
-  <div>
-    <h3></h3>
-    <p><slot name="text" />
+  <div class="like-button">
+    <button>+</button>
+    <span>number</span>
+    <button>-</button>
+  </div>
+  <div class="message-content">
+    <div class="header">
+      <div class="left">
+        <span class="user-name"></span>
+        <span class="message-date"></span>
+      </div>
+      <div class="right">
+        <button>Delete</button>
+        <button>Reply</button>
+      </div>
+    </div>
+    <p><slot name="content"/></p>    
   </div>
 </div>`;
 
 class EmployeeCard extends HTMLElement{
-  static get observedAttributes() { return ['author']; }
+  static get observedAttributes() { return ["author", "created"]; }
 
- constructor(){
-     super();
-     this.attachShadow({ mode: 'open'});
-     this.shadowRoot.appendChild(template.content.cloneNode(true));
-     this._author = this.getAttribute('author');
- } 
+  constructor(){
+    super();
+    this.attachShadow({ mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this._id = this.getAttribute('id');
+    this._author = this.getAttribute('author');
+    this._own = this.getAttribute('own');
+    this._created = this.getAttribute('created');
+    this._score = this.getAttribute('score');
+  } 
 
- attributeChangedCallback(attr, oldVal, newVal) {
-  if (attr === "author") {
-    if (oldVal != newVal) {
-      // Only set this value if it is different
-        this.author = newVal; // Call the setter
-      }
+  attributeChangedCallback(attr, oldVal, newVal) {
+    console.log("attributeChangedCallback", attr, oldVal, newVal);
+  if (oldVal != newVal) {
+    // Only set this value if it is different
+      this[attr] = newVal; // Call the setter
     }
   }
 
   connectedCallback(){
-    this.shadowRoot.querySelector('h3').innerText = this._author;
+    this.shadowRoot.querySelector('span.user-name').innerText = this._author;
+    this.shadowRoot.querySelector('span.message-date').innerText = this._created;
   }
 
   get author() {
-    return this._author; // Return the internal value
+    return this._author;
   }
     
   set author(value) {
-    // Only run this if the new value is different from the internal value
     if (value !== this._author) {
-      console.log(`set author ${value}`);
-      this._author = value; // Set the internal value
-      // If you want the filter property to always show
-      // in the attributes then do this:
-      if (value !== null) {
-          this.shadowRoot.querySelector('h3').innerText = this._author;
-      } else {
-          this.shadowRoot.querySelector('h3').innerText = "";
-      }
+      this._author = value;
+      if (value !== null) this.shadowRoot.querySelector('span.user-name').innerText = this._author;
+      else this.shadowRoot.querySelector('h3').innerText = "";
+    }
+  }
+
+  get created() {
+    return this._created;
+  }
+    
+  set created(value) {
+    console.log("created", value);
+    if (value !== this._created) {
+      this._created = value;
+      if (value !== null) this.shadowRoot.querySelector('span.message-date').innerText = this._created;
+      else this.shadowRoot.querySelector('span.message-date').innerText = "";
     }
   }
 }
